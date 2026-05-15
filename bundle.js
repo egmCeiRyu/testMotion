4f56a5319a29f42943c571752148f95b8affa72d
egmCeiRyu <egami@ceiryu.com>
2026-05-15 12:19:22 +0900

    send

--- bundle.js
+++ bundle.js
@@ -1,1 +1,467 @@
-(()=>{"use strict";var t={91(t,e,n){n.d(e,{A:()=>c});var o=n(645),r=n.n(o),a=n(278),i=n.n(a)()(r());i.push([t.id,"/* Style for \"Tap To Place Model\" text overlay */\n.over {\n  z-index: 10;\n  pointer-events: none;\n  position:absolute;\n  top: 0;\n  left: 0;\n  bottom: 0;\n  right: 0;\n  \n  text-align: center;\n  color: white;\n  font-family: 'Nunito', monospace;\n  text-shadow: 0px 0px 5px black;\n}\n#promptText {\n  font-size: 2em;\n  bottom: 12vh;\n  position: absolute;\n  left: 50%;\n  transform: translate(-50%, 0);\n}\n",""]);const c=i},278(t){t.exports=function(t){var e=[];return e.toString=function(){return this.map(function(e){var n="",o=void 0!==e[5];return e[4]&&(n+="@supports (".concat(e[4],") {")),e[2]&&(n+="@media ".concat(e[2]," {")),o&&(n+="@layer".concat(e[5].length>0?" ".concat(e[5]):""," {")),n+=t(e),o&&(n+="}"),e[2]&&(n+="}"),e[4]&&(n+="}"),n}).join("")},e.i=function(t,n,o,r,a){"string"==typeof t&&(t=[[null,t,void 0]]);var i={};if(o)for(var c=0;c<this.length;c++){var s=this[c][0];null!=s&&(i[s]=!0)}for(var d=0;d<t.length;d++){var l=[].concat(t[d]);o&&i[l[0]]||(void 0!==a&&(void 0===l[5]||(l[1]="@layer".concat(l[5].length>0?" ".concat(l[5]):""," {").concat(l[1],"}")),l[5]=a),n&&(l[2]?(l[1]="@media ".concat(l[2]," {").concat(l[1],"}"),l[2]=n):l[2]=n),r&&(l[4]?(l[1]="@supports (".concat(l[4],") {").concat(l[1],"}"),l[4]=r):l[4]="".concat(r)),e.push(l))}},e}},645(t){t.exports=function(t){return t[1]}},292(t){var e=[];function n(t){for(var n=-1,o=0;o<e.length;o++)if(e[o].identifier===t){n=o;break}return n}function o(t,o){for(var a={},i=[],c=0;c<t.length;c++){var s=t[c],d=o.base?s[0]+o.base:s[0],l=a[d]||0,u="".concat(d," ").concat(l);a[d]=l+1;var p=n(u),f={css:s[1],media:s[2],sourceMap:s[3],supports:s[4],layer:s[5]};if(-1!==p)e[p].references++,e[p].updater(f);else{var v=r(f,o);o.byIndex=c,e.splice(c,0,{identifier:u,updater:v,references:1})}i.push(u)}return i}function r(t,e){var n=e.domAPI(e);return n.update(t),function(e){if(e){if(e.css===t.css&&e.media===t.media&&e.sourceMap===t.sourceMap&&e.supports===t.supports&&e.layer===t.layer)return;n.update(t=e)}else n.remove()}}t.exports=function(t,r){var a=o(t=t||[],r=r||{});return function(t){t=t||[];for(var i=0;i<a.length;i++){var c=n(a[i]);e[c].references--}for(var s=o(t,r),d=0;d<a.length;d++){var l=n(a[d]);0===e[l].references&&(e[l].updater(),e.splice(l,1))}a=s}}},383(t){var e={};t.exports=function(t,n){var o=function(t){if(void 0===e[t]){var n=document.querySelector(t);if(window.HTMLIFrameElement&&n instanceof window.HTMLIFrameElement)try{n=n.contentDocument.head}catch(t){n=null}e[t]=n}return e[t]}(t);if(!o)throw new Error("Couldn't find a style target. This probably means that the value for the 'insert' parameter is invalid.");o.appendChild(n)}},88(t){t.exports=function(t){var e=document.createElement("style");return t.setAttributes(e,t.attributes),t.insert(e,t.options),e}},884(t,e,n){t.exports=function(t){var e=n.nc;e&&t.setAttribute("nonce",e)}},893(t){t.exports=function(t){if("undefined"==typeof document)return{update:function(){},remove:function(){}};var e=t.insertStyleElement(t);return{update:function(n){!function(t,e,n){var o="";n.supports&&(o+="@supports (".concat(n.supports,") {")),n.media&&(o+="@media ".concat(n.media," {"));var r=void 0!==n.layer;r&&(o+="@layer".concat(n.layer.length>0?" ".concat(n.layer):""," {")),o+=n.css,r&&(o+="}"),n.media&&(o+="}"),n.supports&&(o+="}");var a=n.sourceMap;a&&"undefined"!=typeof btoa&&(o+="\n/*# sourceMappingURL=data:application/json;base64,".concat(btoa(unescape(encodeURIComponent(JSON.stringify(a))))," */")),e.styleTagTransform(o,t,e.options)}(e,t,n)},remove:function(){!function(t){if(null===t.parentNode)return!1;t.parentNode.removeChild(t)}(e)}}}},997(t){t.exports=function(t,e){if(e.styleSheet)e.styleSheet.cssText=t;else{for(;e.firstChild;)e.removeChild(e.firstChild);e.appendChild(document.createTextNode(t))}}}},e={};function n(o){var r=e[o];if(void 0!==r)return r.exports;var a=e[o]={id:o,exports:{}};return t[o](a,a.exports,n),a.exports}n.n=t=>{var e=t&&t.__esModule?()=>t.default:()=>t;return n.d(e,{a:e}),e},n.d=(t,e)=>{for(var o in e)n.o(e,o)&&!n.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:e[o]})},n.o=(t,e)=>Object.prototype.hasOwnProperty.call(t,e),n.nc=void 0;var o=n(292),r=n.n(o),a=n(893),i=n.n(a),c=n(383),s=n.n(c),d=n(884),l=n.n(d),u=n(88),p=n.n(u),f=n(997),v=n.n(f),m=n(91),b={};b.styleTagTransform=v(),b.setAttributes=l(),b.insert=s().bind(null,"head"),b.domAPI=i(),b.insertStyleElement=p(),r()(m.A,b),m.A&&m.A.locals&&m.A.locals;var h={init:function(){var t=document.getElementById("ground"),e=document.getElementById("model"),n=document.getElementById("promptText"),o=document.createElement("div");o.innerHTML='\n    <div style="\n      position:fixed;\n      bottom:24px;\n      left:50%;\n      transform:translateX(-50%);\n      z-index:999;\n      display:flex;\n      flex-direction:column;\n      align-items:center;\n      gap:10px;\n      pointer-events:auto;\n    ">\n\n      <div id="scaleLabel" style="\n        color:white;\n        font-size:20px;\n        font-family:Arial;\n        font-weight:bold;\n        text-shadow:0 0 6px black;\n      ">\n        Size: 100%\n      </div>\n\n      <div id="colorMenu" style="\n        display:flex;\n        gap:10px;\n        padding:10px 14px;\n        background:rgba(0,0,0,0.35);\n        border-radius:36px;\n        backdrop-filter:blur(8px);\n      ">\n\n        <button data-color="#ff0000" style="width:46px;height:46px;border-radius:50%;border:3px solid white;background:#ff0000;"></button>\n\n        <button data-color="#00ff00" style="width:46px;height:46px;border-radius:50%;border:3px solid white;background:#00ff00;"></button>\n\n        <button data-color="#0000ff" style="width:46px;height:46px;border-radius:50%;border:3px solid white;background:#0000ff;"></button>\n\n        <button data-color="#ffd700" style="width:46px;height:46px;border-radius:50%;border:3px solid white;background:#ffd700;"></button>\n\n        <button data-color="#ffffff" style="width:46px;height:46px;border-radius:50%;border:3px solid white;background:#ffffff;"></button>\n\n      </div>\n    </div>\n    ',document.body.appendChild(o);var r=document.getElementById("scaleLabel");t.addEventListener("click",function(t){n.style.display="none";var o=t.detail.intersection.point;e.setAttribute("visible","true"),e.setAttribute("position","".concat(o.x," ").concat(o.y," ").concat(o.z))}),document.querySelectorAll("#colorMenu button").forEach(function(t){t.addEventListener("click",function(){var n=t.dataset.color;e.object3D.traverse(function(t){t.isMesh&&t.material&&(t.material=t.material.clone(),t.material.color.set(n))})})}),setInterval(function(){if(e.object3D){var t=e.object3D.scale.x,n=Math.round(t/3*100);r.innerText="Size: ".concat(n,"%")}},120)}};AFRAME.registerComponent("tap-place",h)})();
+(()=>{
+
+"use strict";
+
+var t={
+
+91(t,e,n){
+
+n.d(e,{A:()=>c});
+
+var o=n(645),
+    r=n.n(o),
+    a=n(278),
+    i=n.n(a)()(r());
+
+i.push([
+t.id,
+
+`/* Style for "Tap To Place Model" text overlay */
+
+.over {
+  z-index: 10;
+  pointer-events: none;
+  position:absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  text-align: center;
+  color: white;
+  font-family: 'Nunito', monospace;
+  text-shadow: 0px 0px 5px black;
+}
+
+#promptText {
+  font-size: 2em;
+  bottom: 12vh;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
+
+`,
+""
+]);
+
+const c=i;
+
+},
+
+278(t){
+
+t.exports=function(t){
+
+var e=[];
+
+return e.toString=function(){
+
+return this.map(function(e){
+
+var n="",
+o=void 0!==e[5];
+
+return e[4] &&
+(n+="@supports (".concat(e[4],") {")),
+
+e[2] &&
+(n+="@media ".concat(e[2]," {")),
+
+o &&
+(n+="@layer".concat(
+e[5].length>0?" ".concat(e[5]):"",
+" {"
+)),
+
+n+=t(e),
+
+o && (n+="}"),
+e[2] && (n+="}"),
+e[4] && (n+="}"),
+
+n;
+
+}).join("");
+
+},
+
+e.i=function(t,n,o,r,a){
+
+"string"==typeof t &&
+(t=[[null,t,void 0]]);
+
+var i={};
+
+if(o){
+
+for(var c=0;c<this.length;c++){
+
+var s=this[c][0];
+
+null!=s &&
+(i[s]=!0);
+
+}
+
+}
+
+for(var d=0;d<t.length;d++){
+
+var l=[].concat(t[d]);
+
+o && i[l[0]] ||
+
+(
+
+void 0!==a &&
+(
+void 0===l[5] ||
+
+(
+l[1]="@layer".concat(
+l[5].length>0?" ".concat(l[5]):"",
+" {"
+).concat(l[1],"}")
+),
+
+l[5]=a
+),
+
+n &&
+(
+l[2] ?
+
+(
+l[1]="@media ".concat(
+l[2],
+" {"
+).concat(l[1],"}"),
+
+l[2]=n
+
+)
+
+:
+
+l[2]=n
+),
+
+r &&
+(
+l[4] ?
+
+(
+l[1]="@supports (".concat(
+l[4],
+") {"
+).concat(l[1],"}"),
+
+l[4]=r
+)
+
+:
+
+l[4]="".concat(r)
+),
+
+e.push(l)
+
+);
+
+}
+
+},
+
+e
+
+}
+
+},
+
+645(t){
+
+t.exports=function(t){
+return t[1]
+}
+
+}
+
+},
+
+e={};
+
+function n(o){
+
+var r=e[o];
+
+if(void 0!==r)
+return r.exports;
+
+var a=e[o]={
+id:o,
+exports:{}
+};
+
+return t[o](
+a,
+a.exports,
+n
+),
+
+a.exports;
+
+}
+
+n.n=t=>{
+
+var e=t&&t.__esModule?
+
+()=>t.default
+
+:
+
+()=>t;
+
+return n.d(e,{a:e}),
+e;
+
+};
+
+n.d=(t,e)=>{
+
+for(var o in e)
+
+n.o(e,o) &&
+!n.o(t,o) &&
+
+Object.defineProperty(
+t,
+o,
+{
+enumerable:!0,
+get:e[o]
+}
+);
+
+};
+
+n.o=(t,e)=>
+Object.prototype.hasOwnProperty.call(t,e);
+
+n.nc=void 0;
+
+var h={
+
+init:function(){
+
+var t=document.getElementById("ground"),
+e=document.getElementById("model"),
+n=document.getElementById("promptText"),
+
+o=document.createElement("div");
+
+o.innerHTML=`
+
+<div style="
+  position:fixed;
+  bottom:24px;
+  left:50%;
+  transform:translateX(-50%);
+  z-index:999;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  gap:10px;
+  pointer-events:auto;
+">
+
+  <div id="scaleLabel" style="
+    color:white;
+    font-size:20px;
+    font-family:Arial;
+    font-weight:bold;
+    text-shadow:0 0 6px black;
+  ">
+    Size: 100%
+  </div>
+
+  <div id="colorMenu" style="
+    display:flex;
+    gap:10px;
+    padding:10px 14px;
+    background:rgba(0,0,0,0.35);
+    border-radius:36px;
+    backdrop-filter:blur(8px);
+  ">
+
+    <button
+      data-color="#d9d2c3"
+      style="
+        width:46px;
+        height:46px;
+        border-radius:50%;
+        border:3px solid white;
+        background:#d9d2c3;
+      ">
+    </button>
+
+    <button
+      data-color="#bfa58a"
+      style="
+        width:46px;
+        height:46px;
+        border-radius:50%;
+        border:3px solid white;
+        background:#bfa58a;
+      ">
+    </button>
+
+    <button
+      data-color="#8b6f5a"
+      style="
+        width:46px;
+        height:46px;
+        border-radius:50%;
+        border:3px solid white;
+        background:#8b6f5a;
+      ">
+    </button>
+
+    <button
+      data-color="#5c4435"
+      style="
+        width:46px;
+        height:46px;
+        border-radius:50%;
+        border:3px solid white;
+        background:#5c4435;
+      ">
+    </button>
+
+    <button
+      data-color="#2f241d"
+      style="
+        width:46px;
+        height:46px;
+        border-radius:50%;
+        border:3px solid white;
+        background:#2f241d;
+      ">
+    </button>
+
+  </div>
+
+</div>
+
+`;
+
+document.body.appendChild(o);
+
+var r=
+document.getElementById(
+"scaleLabel"
+);
+
+t.addEventListener(
+"click",
+
+function(t){
+
+n.style.display="none";
+
+var o=
+t.detail.intersection.point;
+
+e.setAttribute(
+"visible",
+"true"
+);
+
+e.setAttribute(
+"position",
+"".concat(
+o.x,
+" ",
+o.y,
+" ",
+o.z
+)
+);
+
+}
+
+);
+
+document
+.querySelectorAll(
+"#colorMenu button"
+)
+
+.forEach(function(t){
+
+t.addEventListener(
+"click",
+
+function(){
+
+var n=
+t.dataset.color;
+
+e.object3D.traverse(
+function(t){
+
+t.isMesh &&
+t.material &&
+
+(
+
+t.material=
+t.material.clone(),
+
+t.material.color.set(n)
+
+);
+
+}
+
+);
+
+}
+
+);
+
+});
+
+setInterval(function(){
+
+if(e.object3D){
+
+var t=
+e.object3D.scale.x,
+
+n=
+Math.round(
+t/.35*100
+);
+
+r.innerText=
+"Size: ".concat(
+n,
+"%"
+);
+
+}
+
+},120);
+
+}
+
+};
+
+AFRAME.registerComponent(
+"tap-place",
+h
+);
+
+})();
