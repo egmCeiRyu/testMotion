27944f319a527d98ce65fbb91707c765b789a195
egmCeiRyu <egami@ceiryu.com>
2026-06-09 12:21:40 +0900

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -2,7 +2,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient(
     "https://vwlevkuhenymqrlhlwdf.supabase.co",
-    "SUA_CHAVE"
+    "sb_publishable_xt00-kBah5vzNhpUNsz1vw_gXzgmL9e"
 );
 
 window.addEventListener("load", async () => {
